e7b1fe4e209c075cd9b304e1892f8c3bedeb9844
Louis Vignier <louis.vignier@supelec.fr>
2020-10-03 08:22:12 +0200

    Initial commit

---
+++ android/src/com/yopox/ld47/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yopox.ld47;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yopox.ld47.LD47;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD47(), config);
+	}
+}
---
+++ desktop/src/com/yopox/ld47/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yopox.ld47.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yopox.ld47.LD47;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD47(), config);
+	}
+}
